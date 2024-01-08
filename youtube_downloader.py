f5fc2d2765ab1de268258ad7e09e85ff4748ab12
Patrick Smith <patricksmith@csu.fullerton.edu>
2024-01-08 02:10:28 -0800

    Porgress on youtube downloader file

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,4 +1,18 @@
 from pytube import YouTube
+import requests
+import os
 
-song_name = " "
-artist_name = " "
+# Youtube Data API 3 key AIzaSyB8vC1kJfXRW_5v0df9JBf4u9uuVwmwUOs
+# Youtube Api being used to get video id from search
+result = " "
+videoId = "XSbZidsgMfw"
+title = "Yonkers"
+url = "https://www.youtube.com/watch?v=" + videoId
+
+# pytube used to download video
+video = YouTube(url)
+video = video.streams.get_highest_resolution()
+video.download(output_path="/home/patrick/Documents/Downloaded_Songs")
+
+# changing file extensions from mp4 to mp3 
+path = "/home/patrick/Documents/Downloaded_Songs"
