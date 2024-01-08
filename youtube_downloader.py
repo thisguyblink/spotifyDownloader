d1bd0a0354a1d7c7ad8f30446ea98e21bc0fefa6
Patrick Smith <patricksmith@csu.fullerton.edu>
2024-01-08 02:46:25 -0800

    APi and Video downloader work

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,13 +1,15 @@
 from pytube import YouTube
-import requests
+import requests 
 import os
 
 # Youtube Data API 3 key AIzaSyB8vC1kJfXRW_5v0df9JBf4u9uuVwmwUOs
 # Youtube Api being used to get video id from search
-result = " "
-videoId = "XSbZidsgMfw"
+data = requests.get('https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8vC1kJfXRW_5v0df9JBf4u9uuVwmwUOs&part=snippet&q=Yonkers Tyler the Creator&type=video')
+results = data.json()
+videoID = results['items'][0]['id']['videoId']
+print(videoID)
 title = "Yonkers"
-url = "https://www.youtube.com/watch?v=" + videoId
+url = "https://www.youtube.com/watch?v=" + videoID
 
 # pytube used to download video
 video = YouTube(url)
@@ -15,4 +17,11 @@ video = video.streams.get_highest_resolution()
 video.download(output_path="/home/patrick/Documents/Downloaded_Songs")
 
 # changing file extensions from mp4 to mp3 
-path = "/home/patrick/Documents/Downloaded_Songs"
+def filesToMp3():
+    path = os.chdir("/home/patrick/Documents/Downloaded_Songs")
+    for song in os.listdir(path):
+        old_name = song.removesuffix("mp4")
+        new_name = song + "mp3"
+        os.rename(song, new_name)
+        print("Song", old_name, "has been converted to a mp3 file")
+
